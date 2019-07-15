f2221dbdcb80de53978349f3f709031343975e53
storres8 <stevetorres7562@gmail.com>
2019-07-15 16:59:46 -0400

    got form to console.log with submit button.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
+  @ViewChild("f", { static: false }) signupForm: NgForm;
 
+  onSubmit() {
+    console.log(this.signupForm);
+  }
 }
