47fef8939a0eb334a83f9045f3646fce402f1aae
storres8 <stevetorres7562@gmail.com>
2019-07-15 17:20:02 -0400

    update.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { NgForm } from "@angular/forms";
 })
 export class AppComponent {
   @ViewChild("f", { static: false }) signupForm: NgForm;
+  defaultSub: "Advanced";
 
   onSubmit() {
     console.log(this.signupForm);
