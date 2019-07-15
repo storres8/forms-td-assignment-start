5ee4eb36867dd7c534670b4abc424b27cb09e1a5
storres8 <stevetorres7562@gmail.com>
2019-07-15 18:47:13 -0400

    done.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from "@angular/core";
+import { Component, ViewChild, OnInit } from "@angular/core";
 import { NgForm } from "@angular/forms";
 
 @Component({
@@ -6,11 +6,22 @@ import { NgForm } from "@angular/forms";
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   @ViewChild("f", { static: false }) signupForm: NgForm;
-  defaultSub: "Advanced";
+  defaultQuestion: "Advanced";
+  user = {
+    email: "",
+    password: "",
+    sub: ""
+  };
+  ngOnInit() {}
 
   onSubmit() {
+    this.user.email = this.signupForm.value.email;
+    this.user.password = this.signupForm.value.password;
+    this.user.sub = this.signupForm.value.subscriptions;
+
     console.log(this.signupForm);
+    console.log(this.user);
   }
 }
